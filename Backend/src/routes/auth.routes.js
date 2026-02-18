482ed9b9ede7d67192ae4db0ed76f80d5b84e6d8
Sameer <sk6586418@gmail.com>
2026-02-18 18:13:17 +0530

    feat: implement login API with JWT authentication

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
-
+const crypto = require("crypto");
 /* 
 * /api/auth/register
 */
@@ -30,6 +31,41 @@ authRouter.post("/register", async (req, res) => {
     res.status(201).json({
         message: "User registered successfully",
         user,
+    });
+});
+
+/* 
+* /api/auth/login
+*/
+authRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User note found with this email address"
+        });
+    }
+
+    const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex");
+
+    if (!isPasswordMatched) {
+        return res.status(401).json({
+            message: "Invalid password"
+        });
+    }
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.cookie("jwt_token", token);
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user,
+        token
     });
 });
 
