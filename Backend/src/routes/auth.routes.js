0b147d2e8bc5344495ec20d3ef8d5d523c8a335f
Sameer <sk6586418@gmail.com>
2026-02-25 23:28:12 +0530

    feat: implement register API with JWT authentication

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -3,24 +3,34 @@ const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
 const crypto = require("crypto");
+
 /* 
 * /api/auth/register
 */
 authRouter.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
+    const { username, email, password, bio, profileImage } = req.body;
 
-    const isUserAlreayExist = await userModel.findOne({ email });
+    const isUserAlreayExist = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    });
 
     if (isUserAlreayExist) {
         return res.status(409).json({
-            message: "User already exists with this email address"
+            message: "User already exists" + (isUserAlreayExist.email == email ? "Email already exists" : "Username already exists")
         });
     }
 
     const hash = crypto.createHash("md5").update(password).digest("hex");
 
     const user = await userModel.create({
-        name, email, password: hash
+        username,
+        email,
+        bio,
+        profileImage,
+        password: hash
     });
 
     const token = jwt.sign({
@@ -30,42 +40,12 @@ authRouter.post("/register", async (req, res) => {
 
     res.status(201).json({
         message: "User registered successfully",
-        user,
-    });
-});
-
-/* 
-* /api/auth/login
-*/
-authRouter.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-
-    const user = await userModel.findOne({ email });
-
-    if (!user) {
-        return res.status(404).json({
-            message: "User note found with this email address"
-        });
-    }
-
-    const isPasswordMatched = user.password === crypto.createHash("md5").update(password).digest("hex");
-
-    if (!isPasswordMatched) {
-        return res.status(401).json({
-            message: "Invalid password"
-        });
-    }
-
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET, { expiresIn: "1h" });
-
-    res.cookie("jwt_token", token);
-
-    res.status(200).json({
-        message: "User logged in successfully",
-        user,
-        token
+        user: {
+            email: user.email,
+            username: user.username,
+            bio: user.bio,
+            profileImage: user.profileImage
+        },
     });
 });
 
